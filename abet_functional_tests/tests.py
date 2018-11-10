694d9d7556e9e95c2315118885381c11b0c30ddb
jdeberg <JeremyDeBerg@gmail.com>
2018-11-10 11:31:27 -0600

    Initial Test to Validate Text Input

--- abet_functional_tests/tests.py
+++ abet_functional_tests/tests.py
@@ -2,7 +2,7 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
-import time
+import time, re
 
 MAX_WAIT = 10
 
@@ -17,5 +17,13 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def test_can_start_a_list_for_one_user(self):
         self.browser.get(self.live_server_url)
-
+        job_title_input = self.browser.find_element_by_id('input_27')
+        job_title_input.send_keys("SOME_JOB")
+        time.sleep(3)
+        submit_button = self.browser.find_element_by_id('input_2')
+        submit_button.click()
+        time.sleep(3)
+        text_found = re.search(r'There are errors on the form. Please fix them before continuing.', self.browser.page_source)
+        # This should be populated as that means the expression above is True
+        self.assertNotEqual(text_found, None)
         time.sleep(3)
