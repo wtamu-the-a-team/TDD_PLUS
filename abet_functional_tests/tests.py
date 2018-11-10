f43ef8809bc2809fe34cf343a5782e46ecf8dd8b
jdeberg <JeremyDeBerg@gmail.com>
2018-11-10 09:55:33 -0600

    slimdown tests and ensure we still load

---
+++ abet_functional_tests/tests.py
@@ -0,0 +1,21 @@
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.common.keys import Keys
+import time
+
+MAX_WAIT = 10
+
+
+class NewVisitorTest(StaticLiveServerTestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_for_one_user(self):
+        self.browser.get(self.live_server_url)
+
+        time.sleep(10)
